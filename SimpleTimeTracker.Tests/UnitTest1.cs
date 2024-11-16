56ddabfb2d2a7ab16cb21a4e7a20936e39c194ee
TheWelshbrit <TheWelshbrit@gmail.com>
2024-11-16 18:13:34 +0000

    adding interfaces and data model for project structure

--- SimpleTimeTracker.Tests/UnitTest1.cs
+++ SimpleTimeTracker.Tests/UnitTest1.cs
@@ -3,8 +3,10 @@ namespace SimpleTimeTracker.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestFramework_IsWorking()
     {
-
+        var expected = 2 + 2;
+        var result = 4;
+        Assert.Equal(expected, result);
     }
 }
