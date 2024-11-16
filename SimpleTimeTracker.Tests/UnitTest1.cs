85fff6d82fdc73f8dce3d3ae8aee2d9535e49b1e
TheWelshbrit <TheWelshbrit@gmail.com>
2024-11-16 16:43:51 +0000

    Initial commit - ASP.NET MVC Project with XUnit Test Project. Task-Prompt included for reference

---
+++ SimpleTimeTracker.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SimpleTimeTracker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ SimpleTimeTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SimpleTimeTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
