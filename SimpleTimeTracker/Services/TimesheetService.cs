cf7d281fb102b4c82c6c71237bd29676f77e04aa
TheWelshbrit <TheWelshbrit@gmail.com>
2024-11-17 19:37:29 +0000

    Updated implementation to ensure validation against bad data in the AddEntry method of the Service class.

--- SimpleTimeTracker/Services/TimesheetService.cs
+++ SimpleTimeTracker/Services/TimesheetService.cs
@@ -20,6 +20,19 @@ namespace SimpleTimeTracker.Services
 
         public void AddEntry(string userName, DateOnly date, string project, string description, double hoursWorked)
         {
+            if (string.IsNullOrEmpty(userName)) { throw new ArgumentException("Invalid UserName"); }
+            if (string.IsNullOrEmpty(project)) { throw new ArgumentException("Invalid Project"); }
+            if (string.IsNullOrEmpty(description)) { throw new ArgumentException("Description"); }
+            
+            if (hoursWorked <= 0) { throw new ArgumentException("Hours Worked must be greater than zero"); }
+            if (hoursWorked > 24) { throw new ArgumentException("Hours Worked cannot exceed a full day"); }
+            
+            var nowDateTime = DateTime.Now;
+            var maxDate = DateOnly.FromDateTime(nowDateTime);
+            var minDate = DateOnly.FromDateTime(nowDateTime.AddMonths(-2));            
+            if (date > maxDate) { throw new ArgumentException("Date cannot be in future."); }
+            if (date < minDate) { throw new ArgumentException("Date cannot be excessively in past."); }
+         
             _repository.AddEntry(new TimesheetEntry{
                 UserName = userName,
                 Date = date,
