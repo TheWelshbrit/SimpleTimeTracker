018e9849414c32a845ebd02d2e4c419564cc3a7d
TheWelshbrit <TheWelshbrit@gmail.com>
2024-11-17 19:11:33 +0000

    Added initial tests to confirm expected behavior of AddEntry method in the service class. Added Moq package to allow for compilation.

--- SimpleTimeTracker.Tests/Services/TimesheetServiceTests.cs
+++ SimpleTimeTracker.Tests/Services/TimesheetServiceTests.cs
@@ -7,6 +7,7 @@ namespace SimpleTimeTracker.Tests.Services
 {
     public class TimesheetServiceTests
     {
+        #region Setup
         private readonly Mock<ITimesheetRepository> _mockRepository;
         private readonly TimesheetService _service;
 
@@ -15,5 +16,42 @@ namespace SimpleTimeTracker.Tests.Services
             _mockRepository = new Mock<ITimesheetRepository>();
             _service = new TimesheetService(_mockRepository.Object);
         }
+        #endregion
+
+        #region AddEntry
+        [Fact]
+        public void AddEntry_ShouldCallRepository_AddEntry_Once()
+        {
+            _service.AddEntry("TestUser", DateOnly.FromDateTime(DateTime.Now), "TestProject", "TestDescription", 5);
+
+            _mockRepository.Verify(repo => 
+                repo.AddEntry(It.IsAny<TimesheetEntry>()),
+                Times.Once
+            );
+        }
+
+        [Fact]
+        public void AddEntry_ShouldCallRepository_WithValidData()
+        {
+            var user = "TestUser";
+            var date = DateOnly.FromDateTime(DateTime.Now);
+            var project = "TestProject";
+            var description = "TestDescription";
+            var hoursWorked = 5;
+
+            _service.AddEntry(user, date, project, description, hoursWorked);
+
+            _mockRepository.Verify(repo => 
+                repo.AddEntry(It.Is<TimesheetEntry>(entry =>
+                    entry.UserName == user &&
+                    entry.Date == date &&
+                    entry.Project == project &&
+                    entry.Description == description &&
+                    entry.HoursWorked == hoursWorked
+                )),
+                Times.Once
+            );
+        }
+        #endregion
     }
 }
