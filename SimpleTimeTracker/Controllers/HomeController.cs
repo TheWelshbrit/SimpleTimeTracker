ef1b5535b72189540e63abd8ce8479d52df15ca9
TheWelshbrit <TheWelshbrit@gmail.com>
2024-11-17 21:49:07 +0000

    Updated HomeController to a barebones outline of the required methods

--- SimpleTimeTracker/Controllers/HomeController.cs
+++ SimpleTimeTracker/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using SimpleTimeTracker.Interfaces;
 using SimpleTimeTracker.Models;
 
 namespace SimpleTimeTracker.Controllers;
@@ -7,25 +8,28 @@ namespace SimpleTimeTracker.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ITimesheetService _timesheetService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ITimesheetService timesheetService)
     {
         _logger = logger;
+        _timesheetService = timesheetService;
     }
-
+    
     public IActionResult Index()
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    
+    [HttpPost]
+    public IActionResult AddEntry(string userName, int year, int month, int day, string project, string description, double hoursWorked)
     {
-        return View();
+        return RedirectToAction("Index");
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpGet]
+    public IActionResult GetCsv()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return null;
     }
 }
